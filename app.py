d4a0a834eb2f4295e76b6f2d8c94f77745b0a4c1
baijiuyang <jiuyang_bai@brown.edu>
2020-11-19 19:42:28 +0000

    Setting up modules

---
+++ app.py
@@ -0,0 +1,7 @@
+'''This Module contains the web application'''
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import plotly.graph_objects as go
