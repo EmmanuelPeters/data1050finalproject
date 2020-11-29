b068e66ede549fac72c1b80fbc19a4d5ccc7c34c
lingyuma1996 <lingyuma1996@gmail.com>
2020-11-29 15:02:06 -0500

    add visualization function in app.py

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
-import plotly.graph_objects as go
+import plotly.express as px
+from os import path
+from PIL import Image
+import numpy as np
+import os
+from scipy.ndimage import gaussian_gradient_magnitude
+from wordcloud import WordCloud, STOPWORDS
+from skimage import io
 
 
 # Definitions of page style constants. Any external CSS stylesheets can go here as well.
@@ -29,7 +36,8 @@ def page_header():
                          style={'height': '35px', 'paddingTop': '10%'})],
                
 
-# Primary text area.               
+# Primary text area.
+
 def description():
     '''
     Project description is returned in markdown
@@ -42,10 +50,34 @@ def description():
         for decision making. Tweeter is a very good place to learn about people’s preferences 
         because it has a huge amount of users and each user’s tweet will reflect what they like and don’t like
         ''',)
-                              
-# Word cloud visualization function
-                              
-def word_cloud():
-                             
-                              
-                              
+
+# Generate word cloud visualiation
+def generate_word_cloud(text):
+    # read the mask image
+    d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+    twitter_image = np.array(Image.open(path.join(d, "twitter_mask.png")))
+
+    # create mask  white is "masked out"
+    twitter_mask = twitter_image.copy()
+    twitter_mask[twitter_mask.sum(axis=2) == 0] = 255
+
+    # some finesse: we enforce boundaries between colors so they get less washed out.
+    # For that we do some edge detection in the image
+    edges = np.mean([gaussian_gradient_magnitude(twitter_mask[:, :, i] / 255., 2) for i in range(3)], axis=0)
+    twitter_mask[edges > .08] = 255
+
+    # the build-in STOPWORDS list will be used, we could more STOPWORDS here.
+    stopwords = set(STOPWORDS)
+
+    wc = WordCloud(background_color="white", max_words=2000, mask=twitter_mask,
+               stopwords=stopwords, contour_width=3, contour_color='steelblue')
+
+    # generate word cloud
+    twitter_wc = wc.generate(text)
+
+    fig = px.imshow(twitter_wc)
+    fig.update_yaxes(visible=False)
+    fig.update_xaxes(visible=False)
+
+    return fig            
+
