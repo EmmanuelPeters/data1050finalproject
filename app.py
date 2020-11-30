3f7e9b26b19055eee5aff71bf59aa13c17fdd012
lingyuma1996 <lingyuma1996@gmail.com>
2020-11-29 21:26:04 -0500

    percent_visualization

--- app.py
+++ app.py
@@ -81,3 +81,21 @@ def generate_word_cloud(text):
 
     return fig            
 
+# Generate a small animation based on the prediction result `perc`
+# The detailed layout should be tuned based on the choose DASH app
+def percentage_visualization(perc):
+    '''the `perc` is the result of previous likelihood prediction based on user's input Tweets'''
+    interval = 20
+    Nframe = int(perc / interval)
+    increase_list = np.linspace(0, perc, Nframe + 1)
+    x_list = []
+    likelihood = []
+    for i in range(Nframe):
+        x_list.append("Tweets")
+        likelihood.append(i * interval)
+    x_list.append("Tweets")
+    likelihood.append(perc)
+
+    fig = px.bar(x = x_list, y = likelihood, animation_frame=increase_list, range_y = [0, 100]) 
+    
+    return fig
