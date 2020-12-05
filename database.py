a2f5dd58ab85dfb80c49725889e47a69a03908d2
baijiuyang <jiuyang_bai@brown.edu>
2020-12-05 17:13:59 -0500

    Fixed data insertion bug

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import logging
 import pymongo
 # ExpriringDict works as a cache. See https://www.pluralsight.com/guides/explore-python-libraries:-in-memory-caching-using-expiring-dict
 from expiringdict import ExpiringDict
-from utils import setup_logger
+from utils import setup_logger, ymdhms
 
 
 client = pymongo.MongoClient(os.environ.get("MONGO"))
@@ -23,14 +23,16 @@ def insert_data(data, collec, many):
     collection = db.get_collection(collec)
     if many:
         collection.insert_many(data)
+        logger.info(f"{ymdhms()} inserted {len(data)} tweets to {collec} collection")
     else:
         collection.insert_one(data)
-    logger.info(f"{data['timestamp']} inserted data {data}")
+        logger.info(f"{ymdhms()} inserted data {data} to {collec} collection")
+    
 
 
 def fetch_all_from_db(collec):
     """
-    Fetch data from MongoDB database as list of dict.
+    Fetch data from MongoDB database as list of dictionary.
     """
     db = client.get_database("tweetstorm")
     collection = db.get_collection(collec)
@@ -38,26 +40,27 @@ def fetch_all_from_db(collec):
     logger.info(str(len(ret)) + ' documents read from the db')
     return ret
 
-def fetch_all(allow_cached=False):
-    """Converts list of dicts returned by `fetch_all_bpa` to DataFrame with ID removed
-    Actual job is done in `_worker`. When `allow_cached`, attempt to retrieve timed cached from
-    `_fetch_all_cache`; ignore cache and call `_work` if cache expires or `allow_cached`
-    is False.
+def fetch_all(collec, allow_cached=False):
+    """
+    Fetch data from MongoDB database as list of dictionary. When `allow_cached`,
+    attempt to retrieve timed cached from `_fetch_all_cache`; ignore cache and 
+    call `_work` if cache expires or `allow_cached` is False.
     """
     def _work():
-        data = fetch_all_from_db()
+        data = fetch_all_from_db(collec)
         if len(data) == 0:
             return None
         return data
 
     if allow_cached:
         try:
-            return _fetch_all_cache['cache']
+            return _fetch_all_cache[collec]
         except KeyError:
             pass
     ret = _work()
-    _fetch_all_cache['cache'] = ret
+    _fetch_all_cache[collec] = ret
     return ret
 
 if __name__ == "__main__":
-    print(fetch_all())
+    print(fetch_all("keywords_frequencies"))
+    print(fetch_all("tweets"))
